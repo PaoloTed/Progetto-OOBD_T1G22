969cfaff87d7c36ac3033972a37a2c28afc6c09d
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-10-31 18:59:46 +0100

    Test Git

--- Object-Orentation/LibreriaOnlineJava/src/main/java/com/example/libreriaonlinejava/HelloApplication.java
+++ Object-Orentation/LibreriaOnlineJava/src/main/java/com/example/libreriaonlinejava/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    //ciao fabrizo
+    //ciao fabrizzios
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
