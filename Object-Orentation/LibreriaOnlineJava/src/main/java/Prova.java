b7f403192cb4660085866cb1cde5f37d919e48ae
fabry40 <fabr.quaranta@studenti.unina.it>
2023-10-31 19:08:23 +0100

    forse va

---
+++ Object-Orentation/LibreriaOnlineJava/src/main/java/Prova.java
@@ -0,0 +1,5 @@
+public class Prova {
+    int fabrizo_sta_provando;
+    int cia9iapofaoiuhfaù;
+    //sfajdhgfjghddjksahbfa
+}
--- Object-Orentation/LibreriaOnlineJava/src/main/java/com/example/libreriaonlinejava/HelloApplication.java
+++ Object-Orentation/LibreriaOnlineJava/src/main/java/com/example/libreriaonlinejava/HelloApplication.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        int a; // pippoASDADA
+        int fabrizio;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
