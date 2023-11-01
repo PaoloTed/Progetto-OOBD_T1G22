03fb170eb2bfac3e5f59d4f83a17ca8397dc2944
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-01 18:58:39 +0100

    Commenti vari

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Main.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Main.java
@@ -12,9 +12,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
+        //Prova per prendere la risoluzione dello schermo e settare la finestra di conseguenza
+        /*
         double w = Screen.getPrimary().getVisualBounds().getWidth();
         double h = Screen.getPrimary().getVisualBounds().getHeight();
+        */
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        //Impostare la finestra non ridimensionabile
         stage.setResizable(false);
 
         stage.setTitle("Benvenuto nella libreria online!");
