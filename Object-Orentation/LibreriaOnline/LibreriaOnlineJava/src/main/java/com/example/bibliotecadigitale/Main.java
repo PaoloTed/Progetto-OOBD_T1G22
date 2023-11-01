61c7e928cf859e3182629a6f857ea2393c2a3e04
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-01 18:51:10 +0100

    Terzo Commit

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Main.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Main.java
@@ -3,6 +3,7 @@ package com.example.bibliotecadigitale;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        double w = Screen.getPrimary().getVisualBounds().getWidth();
+        double h = Screen.getPrimary().getVisualBounds().getHeight();
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setResizable(false);
 
         stage.setTitle("Benvenuto nella libreria online!");
         stage.setScene(scene);
