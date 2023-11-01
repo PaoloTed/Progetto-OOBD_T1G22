30c87e855ca811095a3fcc6bc6cb8f144c414b4a
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-01 18:54:29 +0100

    Aggiustamenti Grafici, resa la schermata non resizable

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -3,7 +3,7 @@ package com.example.bibliotecadigitale.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class welcomeController {
     @FXML
     private Label welcomeText;
 
