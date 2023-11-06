6889beffef43ba78efd061dde372cd7842a5655d
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-06 19:30:25 +0100

    Proòva settaggio contoller

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -1,19 +1,17 @@
 package com.example.bibliotecadigitale.Controller;
 
 import com.example.bibliotecadigitale.Model.Utente;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class welcomeController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    private Utente LoginUtente() {
+    private Button ButtonLoginIn;
+    @FXML
+    private Utente LoginUtente(ActionEvent PressLogin) {
         Utente UtenteLogin= new Utente();
 
 
