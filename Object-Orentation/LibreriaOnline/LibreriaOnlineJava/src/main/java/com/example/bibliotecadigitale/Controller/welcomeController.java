45d6d713f6c78f13bba501405b5e73c203f56d1d
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-06 19:10:10 +0100

    Identificatori bottoni e text

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -1,5 +1,6 @@
 package com.example.bibliotecadigitale.Controller;
 
+import com.example.bibliotecadigitale.Model.Utente;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,12 @@ public class welcomeController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    private Utente LoginUtente() {
+        Utente UtenteLogin= new Utente();
+
+
+        return UtenteLogin;
     }
 }
