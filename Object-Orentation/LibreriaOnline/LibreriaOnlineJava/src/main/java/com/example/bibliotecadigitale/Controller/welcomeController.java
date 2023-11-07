134b4901896209310df06cdbbd776795d3174e77
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-07 17:05:05 +0100

    Cambio scena

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -5,10 +5,16 @@ import com.example.bibliotecadigitale.Connection.Connessione;
 import com.example.bibliotecadigitale.Model.Utente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -49,12 +55,16 @@ public class welcomeController {
         }
     }
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    public Utente SingUpUtente(ActionEvent PressSingUp) {
-
-
-
-        return null;
+    public void SingUpUtente(ActionEvent PressSingUp) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("singUp.fxml"));
+        stage = (Stage)((Node)PressSingUp.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
