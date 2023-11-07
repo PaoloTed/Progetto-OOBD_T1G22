81d277a3b713d6fd78c9a75162de2b5303be3ad6
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-07 17:51:58 +0100

    Aggiunta switch Stage

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -17,6 +17,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
 public class welcomeController {
 
 
@@ -63,7 +64,7 @@ public class welcomeController {
 
         InfoStage myStage = new InfoStage();
 
-        myStage.ChangeStage("singUP.fxml");
+        myStage.SwitchStage("singUP.fxml");
 
     }
 
--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/InfoStage.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/InfoStage.java
@@ -6,7 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Objects;
+
+import static com.example.bibliotecadigitale.Connection.Connessione.className;
 
 public class InfoStage
 {
@@ -14,16 +19,17 @@ public class InfoStage
 
     private Stage stage;
 
-    public void ChangeStage(String S) throws IOException
+    public void SwitchStage(String scenaFxml) throws IOException
     {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(S)));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(scenaFxml)));
         stage = new Stage();
         stage.setScene(new Scene(root, 500, 500));
-        stage.setTitle("Libreria diggitale");
+        stage.setTitle("Libreria digitale");
         stage.setResizable(false);
         stage.show();
     }
 
 
     public Stage getStage() {return stage;}
+
 }
