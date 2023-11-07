af0ee6c790ca0476f900b8f616cecc25b2fd1407
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-07 19:24:58 +0100

    provando a fare la reggistrazione

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/SignUpController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.example.bibliotecadigitale.Controller;
 
+import com.example.bibliotecadigitale.Connection.Connessione;
 import com.example.bibliotecadigitale.InfoStage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,6 +9,12 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class SignUpController
 {
@@ -29,6 +36,21 @@ public class SignUpController
 
     public void signUpDone(ActionEvent event) throws IOException
     {
+        String date = Instant.now().atZone(ZoneOffset.UTC).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        Connection conn = Connessione.getConnection();
+        String emailUser = txtSignUpEmailField.getText();
+        String passwordUser = txtSignUpPasswordField.getText();
+        System.out.println("Connessione effettuata");
+        try {
+            String query = "INSERT INTO Utente VALUES('"+ emailUser +"','"+ passwordUser +"','"+date+"');" ;
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(query);
+
+
+        }catch (Exception e){
+            System.out.println("Errore query test");
+        }
+
         Stage stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
         InfoStage myStage = new InfoStage();
