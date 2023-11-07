a91b1797934fb82780fb8a674c964d6abf7421d3
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-07 16:34:54 +0100

    Test Query sql

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/welcomeController.java
@@ -1,12 +1,17 @@
 package com.example.bibliotecadigitale.Controller;
 
 
+import com.example.bibliotecadigitale.Connection.Connessione;
 import com.example.bibliotecadigitale.Model.Utente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class welcomeController {
 
@@ -24,5 +29,24 @@ public class welcomeController {
         txtEmailField.setText("ciao");
         System.out.println("Login");
         return null;
+    }
+    @FXML
+    public void TestConn(ActionEvent PressLogin) {
+        Connection conn = Connessione.getConnection();
+        String emailUser = txtEmailField.getText();
+        String passwordUser = txtPasswordField.getText();
+        System.out.println("Connessione effettuata");
+        try {
+            String query = "SELECT * FROM utente WHERE email = '" + emailUser + "' AND password = '" + passwordUser + "'";
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery(query);
+            while (rs.next()) {
+                System.out.println("Email: " + rs.getString("email"));
+                System.out.println("Password: " + rs.getString("password"));
+            }
+        }catch (Exception e){
+            System.out.println("Errore query test");
+        }
+
     }
 }
