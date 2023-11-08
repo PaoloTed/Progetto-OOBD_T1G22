7ed58231aed4b389f90a5ed633bf0faaa4c5fa31
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-08 16:47:54 +0100

    aggiusto errore e cambio classe SupportStage

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/SupportStage.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/SupportStage.java
@@ -1,22 +1,25 @@
 package com.example.bibliotecadigitale;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class InfoStage
+public class SupportStage
 {
-    public InfoStage(){}
+    public SupportStage(){}
+
 
     private Stage stage;
 
-    public void SwitchStage(String scenaFxml) throws IOException
+    public void SwitchStage(String scenaNew) throws IOException
     {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(scenaFxml)));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(scenaNew)));
         stage = new Stage();
         stage.setScene(new Scene(root, 500, 500));
         stage.setTitle("Libreria digitale");
@@ -24,13 +27,17 @@ public class InfoStage
         stage.show();
     }
 
-    public void errorStage(String scenaFxml) throws IOException
+    @FXML
+    public TextField textField;
+
+    public void errorStage(String scenaFxml, String tipoErrore) throws IOException
     {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(scenaFxml)));
         stage = new Stage();
         stage.setScene(new Scene(root, 300, 100));
-        stage.setTitle("ErrorController in Libreria digitale");
+        stage.setTitle("Libreria digitale");
         stage.setResizable(false);
+        textField.setText(textField.getText()+":"+tipoErrore);
         stage.show();
     }
 
