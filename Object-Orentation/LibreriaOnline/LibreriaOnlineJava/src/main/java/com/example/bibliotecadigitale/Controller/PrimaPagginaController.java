913b0ef438ce8cc1c1df1d2e49a1b919cb2d4078
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-13 18:32:07 +0100

    inizio PrimaPaggina vers 0.3

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/PrimaPagginaController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/PrimaPagginaController.java
@@ -1,7 +1,11 @@
 package com.example.bibliotecadigitale.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class PrimaPagginaController
 {
-
+@FXML
+     private ComboBox idComboBox;
 
 }
