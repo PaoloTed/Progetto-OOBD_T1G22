6e958f449a1b2541c152a3ae48c69c27c3474344
fabry40 <fabr.quaranta@studenti.unina.it>
2023-11-21 19:19:50 +0100

    LibroDAO Magari funzionasse

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Connection/LibroDAO.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Connection/LibroDAO.java
@@ -1,19 +1,50 @@
 package com.example.bibliotecadigitale.Connection;
 
+import com.example.bibliotecadigitale.Model.Libro;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class LibroDAO {
 
-    public ResultSet searchData(String tipoRicerca, String ricerca) {
+    public ArrayList<String> search(String tipoRicerca, String parolaChiave) {
+        ArrayList<String> isbn = new ArrayList<>();
         ResultSet rs = null;
         try {
             Connessione connessione = new Connessione();
-            String query = "SELECT libro FROM utente WHERE " + tipoRicerca + " = '" + ricerca + "';";
+            String query = "SELECT * FROM libro WHERE " + tipoRicerca + " LIKE '%" + parolaChiave + "%';";
             rs = connessione.executeSearch(query);
+            while (rs.next()) {
+               isbn.add(rs.getString(1));
+            }
+            rs.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return rs;
+        return isbn;
+    }
+
+    public  Libro findLibroFromCodev2(String s)
+    {
+        Libro libroFinded = null;
+        try {
+            Connessione connessione = new Connessione();
+            String query = "SELECT * FROM libro WHERE isbn = '" + s + "';";
+            ResultSet rs = connessione.executeSearch(query);
+            libroFinded = new Libro();
+            while (rs.next()) {
+                libroFinded.setISBN(rs.getString(1));
+                libroFinded.setTitolo(rs.getString(2));
+                libroFinded.setGenere(rs.getString(3));
+                libroFinded.setAutore(rs.getString(10));
+                libroFinded.setEditore(rs.getString(9));
+            }
+            rs.close();
+        } catch (
+                Exception e) {
+            throw new RuntimeException(e);
+        }
+        return libroFinded;
     }
 }
--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/HomeController.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.example.bibliotecadigitale.Controller;
 
 import com.example.bibliotecadigitale.Connection.LibroDAO;
+import com.example.bibliotecadigitale.Model.Libro;
 import com.example.bibliotecadigitale.SupportStage;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
@@ -20,13 +23,30 @@ public class HomeController implements Initializable {
     @FXML
     private TextField idBarSearch;
     public SupportStage support = new SupportStage();
+    @FXML
+    public ListView<String> listViewLibri;
 
     @FXML
     void Select(ActionEvent event) {
+        LibroDAO libroDAO = new LibroDAO();
         String scelta =idComboBox.getSelectionModel().getSelectedItem();
         System.out.println(scelta);
-        LibroDAO libroDAO = new LibroDAO();
-        ResultSet rs = libroDAO.searchData(scelta,idBarSearch.getText());
+        ArrayList<String> isbn = libroDAO.search(scelta,idBarSearch.getText());
+        Libro libro = new Libro();
+        String titolo;
+        String autore;
+        String genere;
+        String editore;
+        String isbnAppoggio;
+        for (int i = 0; i < isbn.size(); i++) {
+            libro = libroDAO.findLibroFromCodev2(isbn.get(i));
+            titolo = libro.getTitolo();
+            autore = libro.getAutore();
+            genere = libro.getGenere();
+            editore = libro.getEditore();
+            isbnAppoggio = libro.getISBN();
+            listViewLibri.getItems().add(isbnAppoggio + " - " + titolo + " - " + autore + " - " + genere + " - " + editore);
+        }
     }
     @FXML
     public void goToInfoUser(ActionEvent event) {
