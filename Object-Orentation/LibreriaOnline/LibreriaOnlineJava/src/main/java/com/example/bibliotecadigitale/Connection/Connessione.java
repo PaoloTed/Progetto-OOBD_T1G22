ff9d3742f515abd97086937455e107334fea1482
PaoloTed <tedesco.paolo.tp@gmail.com>
2023-11-21 16:02:20 +0100

    test 2

--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Connection/Connessione.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Connection/Connessione.java
@@ -1,25 +1,42 @@
 package com.example.bibliotecadigitale.Connection;
+
+import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.filechooser.FileSystemView;
 
 public class Connessione {
-    public static String url ="";
+    public static String url = "";
     public static String className = "";
     public static String user = "";
     public static String password = "";
     private static Connection con;
 
-    static{
-        try{
+
+    static {
+        try {
             Class.forName(className);
-        }catch(ClassNotFoundException e1){
+        } catch (ClassNotFoundException e1) {
         }
         //connessione alla base di dati libreriaOnline
         try {
-            con = DriverManager.getConnection(url,user,password);//forse segnalare al prof di aggiungere il drive al progetto
+            try {
+                //leggo le credenziali dal file credenzialiProgetto.txt
+                //il file deve essere nella cartella documenti
+                ArrayList<String> credenziali = leggiCredenziali();
+                url = credenziali.get(0);
+                className = credenziali.get(1);
+                user = credenziali.get(2);
+                password = credenziali.get(3);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            con = DriverManager.getConnection(url, user, password);//forse segnalare al prof di aggiungere il drive al progetto
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
     public static Connection getConnection() {
         return con;
     }
@@ -28,6 +45,19 @@ public class Connessione {
         Statement stat = con.createStatement();
         ResultSet rs = stat.executeQuery(query);
         return rs;
+    }
+
+    public static ArrayList<String> leggiCredenziali() throws IOException {
+        String pathDocumenti = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+        File file = new File(pathDocumenti + "\\credenzialiProgetto.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        ArrayList<String> credenzialiAppoggio = new ArrayList<>();
+        String st;
+        while ((st = br.readLine()) != null) {
+            credenzialiAppoggio.add(st);
+        }
+        return credenzialiAppoggio;
+
     }
 
     public void executeUpdate(String query) throws SQLException {
--- Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/module-info.java
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.libreriaonlinejava {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     exports com.example.bibliotecadigitale.Controller;
