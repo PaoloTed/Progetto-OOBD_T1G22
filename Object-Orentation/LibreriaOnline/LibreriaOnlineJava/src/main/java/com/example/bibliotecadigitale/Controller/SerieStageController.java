fa7c38c2b7cc2a10494a3d6e642b3aec49bc06f2
fabry40 <fabr.quaranta@studenti.unina.it>
2024-01-23 17:41:30 +0100

    Serie, aggiungere legame con support stage e testare

---
+++ Object-Orentation/LibreriaOnline/LibreriaOnlineJava/src/main/java/com/example/bibliotecadigitale/Controller/SerieStageController.java
@@ -0,0 +1,91 @@
+package com.example.bibliotecadigitale.Controller;
+
+import com.example.bibliotecadigitale.Connection.*;
+import com.example.bibliotecadigitale.Model.*;
+import com.example.bibliotecadigitale.SupportStage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SerieStageController implements Initializable {
+
+    public SupportStage support = new SupportStage();
+
+
+    @FXML
+    private Text codSTxtId;
+    @FXML
+    private Text nomeTxtId;
+    @FXML
+    private Text numLibTxtId;
+    @FXML
+    private Text completaTxtId;
+
+    @FXML
+    public TableView<Libro> libroTableView;
+
+    @FXML
+    TableColumn<Libro, String> isbnColumn;
+    @FXML
+    TableColumn<Libro, String> titoloColumn;
+    @FXML
+    TableColumn<Libro, String> autoreColumn;
+    @FXML
+    TableColumn<Libro, String> editoreColumn;
+    @FXML
+    TableColumn<Libro, String> genereColumn;
+
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //inizializzo le colonne della tabella libro
+
+        isbnColumn.setCellValueFactory(new PropertyValueFactory<Libro, String>("ISBN"));
+        titoloColumn.setCellValueFactory(new PropertyValueFactory<Libro, String>("titolo"));
+        autoreColumn.setCellValueFactory(new PropertyValueFactory<Libro, String>("autore"));
+        editoreColumn.setCellValueFactory(new PropertyValueFactory<Libro, String>("editore"));
+        genereColumn.setCellValueFactory(new PropertyValueFactory<Libro, String>("genere"));
+
+    }
+
+    public void showSerie(String CodS)
+    {
+        SerieDAOImpl serieDAO = new SerieDAOImpl();
+        Serie serie = serieDAO.get(CodS);
+        codSTxtId.setText(String.valueOf(serie.getCodS()));
+        nomeTxtId.setText(serie.getNome());
+        numLibTxtId.setText(String.valueOf(serie.getNumLibri()));
+        if (serie.getCompletata())
+            completaTxtId.setText("Completata");
+        else
+            completaTxtId.setText("No Terminata");
+
+        LibroDAOImpl libroDAO = new LibroDAOImpl();
+        ArrayList<Libro> libri = libroDAO.getRicerca("serie", CodS);
+        libroTableView.getItems().setAll(libri);
+
+
+
+
+    }
+
+
+    public void close(ActionEvent event) {
+        SupportStage support = new SupportStage();
+        Stage stage = (Stage) codSTxtId.getScene().getWindow();
+        stage.close();
+
+    }
+
+
+}
